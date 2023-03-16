e4c2b16fd0af514bdcc26c24db500d9d8e24d56d
loc <tloc32983@gmail.com>
2023-03-16 20:45:52 +0700

    add

---
+++ TranThanhLocwebsite/Webbanhang/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webbanhang.Context;
+
+namespace Webbanhang.Controllers
+{
+    public class ProductController : Controller
+    {
+        locEntities objlocEntities = new locEntities();
+        // GET: Product
+        public ActionResult Detail(int Id)
+        {
+            var objProduct = objlocEntities.Products.Where(n => n.Id==Id).FirstOrDefault();
+            return View(objProduct);
+        }
+    }
+}
